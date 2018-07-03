97cc2bb6033a6d47f3091420730dce52267d3238
gaku3601 <pro.gaku@gmail.com>
2018-07-03 17:47:59 +0900

    複数の戻り値があるパターンを実装

--- study2/README.md
+++ study2/README.md
@@ -1,2 +1,17 @@
 # 概要
 rangeで処理するパターン。  
+
+## ポイント
+
+下部、
+
+    for v := range valueChan {
+        fmt.Println(v.value)
+    }
+    for v := range errChan { ←ここは届かないのでdaedlock
+        fmt.Println(v.value)
+    }
+
+rangeの場合、上記のように実装すると、errChanのrange部分に到達することができないので、daedlockとなる。  
+また、rangeの場合、明示的にclose(chan)を呼び出して上げないと、これもdaedlock。  
+複数の戻り値がある場合、structでまとめて返却してあげればいい感じに返却できる。  
--- study2/main.go
+++ study2/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"sync"
 	"time"
 )
 
+type data struct {
+	err   error
+	value int
+}
+
 func main() {
-	valueChan := make(chan int)
+	// ここは*dataよりdataの方がコストが小さい
+	valueChan := make(chan data)
 	wg := new(sync.WaitGroup)
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
@@ -22,11 +29,19 @@ func main() {
 		close(valueChan)
 	}()
 	for v := range valueChan {
-		fmt.Println(v)
+		fmt.Println(v.err)
+		fmt.Println(v.value)
+		if v.err != nil {
+			return
+		}
 	}
 }
 
-func process(i int, valueChan chan<- int) {
+func process(i int, valueChan chan<- data) {
+	if i == 50 {
+		valueChan <- data{err: errors.New("error"), value: 0}
+		return
+	}
 	time.Sleep(1 * time.Second)
-	valueChan <- i
+	valueChan <- data{err: nil, value: i}
 }
